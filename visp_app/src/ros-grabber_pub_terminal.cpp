21af0cb8fa6f9b598f6e3999dc9a3b06832da23b
Andrey Ch <63464663+Ader01@users.noreply.github.com>
2020-10-19 17:19:40 +0300

    Add files via upload

---
+++ visp_app/src/ros-grabber_pub_terminal.cpp
@@ -0,0 +1,95 @@
+//ros-grabber_pub_terminal.cpp (no window, only terminal version)
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+#include <visp/vpDisplayX.h>
+#include <visp/vpImage.h>
+#include <visp_ros/vpROSGrabber.h>
+#include <visp3/core/vpConfig.h>
+#include <visp3/core/vpImageConvert.h>
+#include <visp3/detection/vpDetectorDataMatrixCode.h>
+#include <visp3/detection/vpDetectorQRCode.h>
+#include <visp3/gui/vpDisplayGDI.h>
+#include <visp3/gui/vpDisplayOpenCV.h>
+#include <visp3/gui/vpDisplayX.h>
+#include <visp3/sensor/vpV4l2Grabber.h>
+
+#include <my_topics/QrLabelInfo.h> 
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "publisher");
+    ros::NodeHandle n;
+    ros::Publisher pub = n.advertise<my_topics::QrLabelInfo>("/publisher_topic", 10);
+    ros::Rate loop_rate(10);
+
+    my_topics::QrLabelInfo qr_data; //создание обьекта класса my_topics::QrLabelInfo с именем qr_data         
+    
+    std::cout << "visp_app ros-grabber_pub" << std::endl;
+    bool opt_use_camera_info;
+    vpImage<unsigned char> I; // Create a color image container
+
+    vpROSGrabber g; // Create a grabber based on ROS
+    g.setImageTopic("/simple_cam/camera1/image_raw");//! [Setting camera topic]
+    if (opt_use_camera_info) 
+    {
+      g.setCameraInfoTopic("/simple_cam/camera1/camera_info");//! [Setting camera info]
+      g.setRectify(true);
+    }
+    g.open(I);//! [Opening]
+    std::cout << "Image size: " << I.getWidth() << " " << I.getHeight() << std::endl;
+
+
+  //barcode detector process
+  vpDetectorBase *detector = NULL;
+  detector = new vpDetectorQRCode;
+
+  while(1){ //цикл прорисовки и распознавания
+
+    vpDisplay::display(I);
+    bool status = detector->detect(I);
+
+    if (status) {
+      for (size_t i = 0; i < detector->getNbObjects(); i++) {
+        std::vector<vpImagePoint> p = detector->getPolygon(i);
+        vpRect bbox = detector->getBBox(i);
+
+        std::cout << std::endl << "bbox= " << bbox << std::endl;
+        std::cout << "bbox.getTop= " << bbox.getTop() << std::endl;
+        std::cout << "bbox.getLeft= " << bbox.getLeft() << std::endl;
+        std::cout << "bbox.getWidth= " << bbox.getWidth() << std::endl; 
+        std::cout << "bbox.getHeight= " << bbox.getHeight() << std::endl; 
+       
+        qr_data.text = detector->getMessage(i); //запись считанного сообщения в qr_data.text
+        std::cout << "Message qr_data: " << qr_data.text << std::endl; // вывод считанного сообщения в терминал
+
+        std::cout << "координаты центра bbox.getCenter = "<< bbox.getCenter() << std::endl; // вывод координат центра маркера
+      
+        vpImagePoint pc = bbox.getCenter(); //запись координат центра QR кода в qr_data
+        qr_data.center.x = pc.get_i(); 
+        qr_data.center.y = pc.get_j();
+
+        std::cout << "координаты вершин QR кода " << std::endl; // вывод координат вершин QR кода
+        for (size_t j = 0; j < p.size(); j++) {
+        std::cout << "p[" << j << "]= " << p[j] << std::endl; //вывод в терминал координат вершин
+        }
+
+        pub.publish(qr_data); //отправка данных в топик
+        std::cout << "qr_data  published!" << std::endl;
+        vpDisplay::displayText(I, (int)bbox.getTop() - 10, (int)bbox.getLeft(), "qr_data  published!", vpColor::yellow);
+        ros::spinOnce();
+        loop_rate.sleep();
+      }
+    }
+  }// end VISP
+
+
+
+  return 0; //end programm
+}
+
